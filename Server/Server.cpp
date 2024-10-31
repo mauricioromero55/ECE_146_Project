64481a8c3bb04b3b25a03cd503f18fe371df3a03
mauricioromero55 <64935772+mauricioromero55@users.noreply.github.com>
2024-10-31 10:48:01 -0700

    Update Server.cpp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,80 +1,131 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <mutex>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
-#pragma comment(lib, "ws2_32.lib")  // Link with Winsock library
+#pragma comment(lib, "ws2_32.lib") // Link Winsock library
 
-void error(const char *msg)
-{
-    perror(msg);
-    WSACleanup();  // Clean up Winsock resources on error
-    exit(1);
+using namespace std;
+
+#define PORT 9090
+#define MAX_CLIENTS 10
+
+vector<SOCKET> clients; // To store client sockets
+mutex client_mutex;      // Mutex for thread-safe access to client list
+
+// Function to handle each client
+void handle_client(SOCKET client_socket) {
+    char buffer[1024] = { 0 };
+    while (true) {
+        // Clear the buffer
+        memset(buffer, 0, sizeof(buffer));
+
+        // Read message from client
+        int valread = recv(client_socket, buffer, sizeof(buffer), 0);
+
+        if (valread <= 0) {
+            // Client has disconnected
+            cout << "Client disconnected.\n";
+            closesocket(client_socket);
+
+            // Remove client from list
+            client_mutex.lock();
+            clients.erase(remove(clients.begin(), clients.end(), client_socket), clients.end());
+            client_mutex.unlock();
+            break;
+        }
+
+        // Print received message
+        cout << "Message from client: " << buffer << endl;
+
+        // Forward the message to all other clients
+        client_mutex.lock();
+        for (SOCKET client : clients) {
+            if (client != client_socket) {
+                send(client, buffer, strlen(buffer), 0);
+            }
+        }
+        client_mutex.unlock();
+    }
 }
 
-int main(int argc, char *argv[])
-{
+int main() {
     WSADATA wsaData;
     int iResult;
 
     // Initialize Winsock
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
-        fprintf(stderr, "WSAStartup failed with error: %d\n", iResult);
-        exit(1);
+        cerr << "WSAStartup failed: " << iResult << endl;
+        return 1;
     }
 
-    SOCKET sockfd, newsockfd;
-    int portno;
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
-    socklen_t clilen;
-    char buffer[256];
+    SOCKET server_fd, client_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
 
-    if (argc < 2) {
-        fprintf(stderr, "ERROR, no port provided\n");
+    // Create server socket
+    server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_fd == INVALID_SOCKET) {
+        cerr << "Socket creation failed: " << WSAGetLastError() << endl;
         WSACleanup();
-        exit(1);
+        return 1;
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == INVALID_SOCKET)
-        error("ERROR opening socket");
+    // Set socket options to allow address reuse
+    iResult = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt));
+    if (iResult == SOCKET_ERROR) {
+        cerr << "Setsockopt failed: " << WSAGetLastError() << endl;
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
 
-    memset(&serv_addr, 0, sizeof(serv_addr));  // Zero out the server address structure
-    portno = atoi(argv[1]);  // Convert the port number from a string to an integer
+    // Bind the socket to the IP and port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    iResult = bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    if (iResult == SOCKET_ERROR) {
+        cerr << "Bind failed: " << WSAGetLastError() << endl;
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
 
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR)
-        error("ERROR on binding");
+    // Listen for incoming connections
+    iResult = listen(server_fd, MAX_CLIENTS);
+    if (iResult == SOCKET_ERROR) {
+        cerr << "Listen failed: " << WSAGetLastError() << endl;
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+    cout << "Server listening on port " << PORT << endl;
 
-    listen(sockfd, 5);  // Start listening with a backlog of 5 connections
-    clilen = sizeof(cli_addr);
+    // Accept connections in a loop
+    while (true) {
+        client_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
+        if (client_socket == INVALID_SOCKET) {
+            cerr << "Accept failed: " << WSAGetLastError() << endl;
+            continue;
+        }
+        cout << "New client connected.\n";
 
-    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
-    if (newsockfd == INVALID_SOCKET)
-        error("ERROR on accept");
+        // Add new client to the list and create a new thread to handle it
+        client_mutex.lock();
+        clients.push_back(client_socket);
+        client_mutex.unlock();
+        thread(handle_client, client_socket).detach();  // Detach thread to allow independent execution
+    }
 
-    memset(buffer, 0, 256);  // Clear the buffer
-
-    n = recv(newsockfd, buffer, 255, 0);  // Receive message from the client
-    if (n == SOCKET_ERROR) error("ERROR reading from socket");
-
-    printf("Here is the message: %s\n", buffer);
-
-    n = send(newsockfd, "I got your message", 18, 0);  // Send response to client
-    if (n == SOCKET_ERROR) error("ERROR writing to socket");
-
-    closesocket(newsockfd);
-    closesocket(sockfd);
-    WSACleanup();  // Clean up Winsock
-
+    // Clean up Winsock resources
+    closesocket(server_fd);
+    WSACleanup();
     return 0;
 }
-
